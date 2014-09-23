6f7c8e79980b6a04cc6d4aeb4040f10ba0a8e0bb
gstearmit@gmail.com <gstearmit@gmail.com@686fdb54-8680-49d5-c479-c5e3a3cd88d6>
2014-09-23 03:03:33 +0000

    fix lai quang cao banner new

--- public/advnew/adsAppnew.js
+++ public/advnew/adsAppnew.js
@@ -13,7 +13,7 @@ $(document).ready(function() {
      dowloadapp +=' <div class="box_div_right_view">'+
 	    '<h3 class="title_h1_st2">Mời Tải ứng dụng</h3>'+
 	     '<div class="tn-hhapp-bx">'+
-	        '<a class="apple" target="_blank" href="https://play.google.com/store/apps/details?id=com.playboyviet.alatca.vn"><img src="http://playboyviet.com/public/advnew/icon-downloadapp.gif" width="100" height="300" alt="appstore"></a>' +
+	        '<a class="apple" target="_blank" href="https://play.google.com/store/apps/details?id=com.playboyviet.alatca.vn"><img src="http://playboyviet.com/public/advnew/icon-downloadapp.gif" width="100" height="29px" alt="appstore"></a>' +
 	        '<a class="apple" target="_blank" href="https://play.google.com/store/apps/details?id=com.playboyviet.alatca.vn"><img src="http://playboyviet.com/public/advnew/appstore-app.png" width="60" height="64" alt="appstore"></a>' +
 	         '<a class="android" target="_blank" href="https://play.google.com/store/apps/details?id=com.playboyviet.alatca.vn"><img src="http://playboyviet.com/public/advnew/playstore-app.png" width="60" height="64" alt=""></a>'+
 	         '<a class="window" target="_blank" href="http://www.windowsphone.com/en-us/store/app/playboyviet/2820e163-f66e-4cae-823d-ea256716d385/xap?apptype=regular"><img src="http://playboyviet.com/public/advnew/windowsphone-app.png" width="64" height="64" alt=""></a>'
